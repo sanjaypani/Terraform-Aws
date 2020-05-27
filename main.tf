002bfd0d889a8a334b7909a08078b3742f2f7f14
Sanjay Pani <sanjaykpani@gmail.com>
2020-05-27 16:45:55 +0530

    Terraform exe file & String & Map Operation.
    Signed-off-by: Sanjay Pani <sanjaykpani@gmail.com>

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "admin" {
+	type = string
+	default = "Hello Terafform......"
+}
+
+variable "mymap" {
+	type = map(string)
+	default = {
+		mykey = "Bangalore"
+	}
+}
+
+variable "mymap" {
+	type = map(string)
+	default = {
+		mykey = "Bangalore"
+	}
+}
